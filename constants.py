52f06d2fcbcaab7cb325eb00cdd250488a6c4691
khang Le <nguyen.khang.le@mths.ca>
2019-10-30 18:41:20 +0000

    add file

---
+++ constants.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Created by: Joey Marcotte
+# Created on: September 2019
+# This is contains constants
+
+
+SCREEN_X = 160
+SCREEN_Y = 120
+SCREEN_GRID_X = 16
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ = 0
+FPS = 60
+
+# button
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released",
+}
